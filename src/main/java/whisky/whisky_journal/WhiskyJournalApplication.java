459e51a28e399dfba3787b852d08ed5d0f50ca66
brian <brian506@naver.com>
2025-10-26 14:56:45 +0900

    init

---
+++ src/main/java/whisky/whisky_journal/WhiskyJournalApplication.java
@@ -0,0 +1,13 @@
+package whisky.whisky_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/whisky/whisky_journal/WhiskyJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package whisky.whisky_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
