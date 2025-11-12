36c84bfbcbc8dd68264decc0cf5a4257defd7d50
space <kh3367@naver.com>
2025-11-12 16:57:16 +0900

    FEAT : Keycloak 로그인 기능 추가

---
+++ src/main/java/whisky/whisky_journal/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package whisky.whisky_journal.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("whisky-journal Swagger")
+                .description("whisky-journal 관한 Rest API")
+                .version("1.0.0");
+    }
+}
